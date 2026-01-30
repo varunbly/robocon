d94f72f07243276cadcf18c65aae7cc6a26113b9
shivam-165 <shivam24529@iiitd.ac.in>
2026-01-30 08:48:07 +0530

    r1 on joint plugin

--- src/robocon_application/robocon_application/r1_teleop.py
+++ src/robocon_application/robocon_application/r1_teleop.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
 import sys
 import termios
 import tty
@@ -22,15 +23,29 @@ Controls:
 class R1Teleop(Node):
     def __init__(self):
         super().__init__('r1_teleop')
-        self.pub = self.create_publisher(Twist, '/R1/cmd_vel', 10)
+        self.pub_fl = self.create_publisher(
+            Float64, '/model/R1/joint/front_left_wheel_joint/cmd_vel', 10)
+        self.pub_bl = self.create_publisher(
+            Float64, '/model/R1/joint/back_left_wheel_joint/cmd_vel', 10)
+        self.pub_fr = self.create_publisher(
+            Float64, '/model/R1/joint/front_right_wheel_joint/cmd_vel', 10)
+        self.pub_br = self.create_publisher(
+            Float64, '/model/R1/joint/back_right_wheel_joint/cmd_vel', 10)
+
         self.settings = termios.tcgetattr(sys.stdin)
 
     def get_key(self):
         tty.setraw(sys.stdin.fileno())
-        select.select([sys.stdin], [], [], 0)
-        key = sys.stdin.read(1)
+        key = sys.stdin.read(1)   # BLOCKS until key is pressed
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
         return key
+
+    
+    def publish_wheels(self, fl, bl, fr, br):
+        self.pub_fl.publish(Float64(data=fl))
+        self.pub_bl.publish(Float64(data=bl))
+        self.pub_fr.publish(Float64(data=fr))
+        self.pub_br.publish(Float64(data=br))
 
 def main():
     rclpy.init()
@@ -42,22 +57,21 @@ def main():
         while True:
             key = node.get_key()
             twist = Twist()
-            
+            fl=bl=fr=br=0.0
             if key == 'w':
-                twist.linear.x = 0.5
+                fl=bl=fr=br=5.0
             elif key == 's':
-                twist.linear.x = -0.5
+                fl=bl=fr=br=-5.0
             elif key == 'a':
-                twist.angular.z = 2.5  # Increased > 2x (was 1.0)
+                fl=bl=-5.0
+                fr=br=5.0
             elif key == 'd':
-                twist.angular.z = -2.5 # Increased > 2x (was 1.0)
-            elif key == 'x':
-                twist.linear.x = 0.0
-                twist.angular.z = 0.0
-            elif key == 'q':
+                fl=bl=5.0
+                fr=br=-5.0
+            elif key == 'q':    
                 break
 
-            node.pub.publish(twist)
+            node.publish_wheels(fl,bl,fr,br)
 
     except Exception as e:
         print(e)
